4c187b0ed47c61ed4d5bec9a7074edf756be6cb2
Shaun Robinson <connecshaun@gmail.com>
2021-11-19 03:14:38 +0000

    Add in FetchISSFlyOverTimes Function which called in yet another API

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 // const { fetchMyIP } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 
 // fetchMyIP((error, ip) => {
@@ -12,21 +14,25 @@
 
 // });
 
-// fetchCoordsByIP(Myip,(error, description) => {
-//   if(error) {
-//     console.log("Error:", error);
-//   } else {
-//     console.log(description);
+
+// fetchCoordsByIP('154.20.190.226', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
 //   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
 // });
 
-const { fetchCoordsByIP } = require('./iss');
 
-fetchCoordsByIP('154.20.190.226', (error, coordinates) => {
+const exampleCoords = { latitude: '49.2773', longitude: '-123.0679' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned coordinates:' , coordinates);
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
+
